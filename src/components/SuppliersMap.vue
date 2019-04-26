9c06d18e3c17aa34ade9c49e5e4228e81872e6f8
micallefN <nicolas.micallef@hotmail.com>
2019-04-26 17:05:52 +0200

    ajout map google

--- src/components/SuppliersMap.vue
+++ src/components/SuppliersMap.vue
@@ -1,10 +1,19 @@
 <template>
-  <h1>Carte des fournisseurs</h1>
+  <div>
+    <h1>Carte des fournisseurs</h1>
+    <gmap-map
+            :center="{lat:45.19, lng:5.72}"
+            :zoom="7"
+            style="width: 100%; height: 800px"
+    >
+    </gmap-map>
+  </div>
+
 </template>
 
 <script>
   export default {
-    name: 'SuppliersMap'
+    name: 'SuppliersMap',
   }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 
 import {router} from './router/index.js'
 
+import * as VueGoogleMaps from 'vue2-google-maps'
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyCAa4lYFQ6RxfebUMmALUHtS-1UvzIW3C0',
+  }
+})
+
 Vue.config.productionTip = false
 
 new Vue({
