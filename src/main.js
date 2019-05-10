cee0cde498c49d98aba48e44c66582be835c3cc3
micallefN <nicolas.micallef@hotmail.com>
2019-05-10 10:07:18 +0200

    ajout env local pour apikey

--- src/main.js
+++ src/main.js
@@ -6,13 +6,14 @@ import {router} from './router/index.js'
 import * as VueGoogleMaps from 'vue2-google-maps'
 Vue.use(VueGoogleMaps, {
   load: {
-    key: 'AIzaSyCAa4lYFQ6RxfebUMmALUHtS-1UvzIW3C0',
-  }
-})
 
-Vue.config.productionTip = false
+    key: process.env.VUE_APP_APIKEY,
+  }
+});
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
